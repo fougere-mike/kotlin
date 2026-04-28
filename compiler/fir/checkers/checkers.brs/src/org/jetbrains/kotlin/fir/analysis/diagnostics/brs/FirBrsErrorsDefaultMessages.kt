3d7b1ad33af989364f90fc76665b2991c3473f1f
Mike Fougere <fougere.mike@gmail.com>
2026-04-28 11:53:17 -0400

    BRS_BRSCONSTANT_NON_OBJECT: register default message (B2)

--- compiler/fir/checkers/checkers.brs/src/org/jetbrains/kotlin/fir/analysis/diagnostics/brs/FirBrsErrorsDefaultMessages.kt
+++ compiler/fir/checkers/checkers.brs/src/org/jetbrains/kotlin/fir/analysis/diagnostics/brs/FirBrsErrorsDefaultMessages.kt
@@ -9,6 +9,7 @@ import org.jetbrains.kotlin.diagnostics.KtDiagnosticFactoryToRendererMap
 import org.jetbrains.kotlin.diagnostics.rendering.BaseDiagnosticRendererFactory
 import org.jetbrains.kotlin.diagnostics.rendering.CommonRenderers
 import org.jetbrains.kotlin.fir.analysis.diagnostics.brs.FirBrsErrors.BRS_ADDFIELD_INVALID_TYPE
+import org.jetbrains.kotlin.fir.analysis.diagnostics.brs.FirBrsErrors.BRS_BRSCONSTANT_NON_OBJECT
 import org.jetbrains.kotlin.fir.analysis.diagnostics.brs.FirBrsErrors.BRS_BRSCONSTANT_VAR
 import org.jetbrains.kotlin.fir.analysis.diagnostics.brs.FirBrsErrors.BRS_BRSNAME_REQUIRES_CALLABLE_REF
 import org.jetbrains.kotlin.fir.analysis.diagnostics.brs.FirBrsErrors.BRS_CREATE_OBJECT_INVALID_TYPE
@@ -98,6 +99,11 @@ object FirBrsErrorsDefaultMessages : BaseDiagnosticRendererFactory() {
         map.put(
             BRS_BRSCONSTANT_VAR,
             "@BrsConstant objects cannot contain var properties. Use val instead: ''{0}''.",
+            CommonRenderers.STRING,
+        )
+        map.put(
+            BRS_BRSCONSTANT_NON_OBJECT,
+            "@BrsConstant is only valid on object declarations. Found on {0} declaration.",
             CommonRenderers.STRING,
         )
     }
